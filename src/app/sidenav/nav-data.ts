779ae8dae813d254fb3c18af177f953c999c3801
Sellami <sofien.sellami@esprit.tn>
2022-03-23 23:03:30 +0100

    NavBar

---
+++ src/app/sidenav/nav-data.ts
@@ -0,0 +1,18 @@
+export const navbarData = [
+  {
+    routeLink :'dashboard',
+    icon : 'fal fa-home',
+    label : 'Dashboard'
+  },
+  {
+    routeLink :'products',
+    icon : 'fal fa-map',
+    label : 'Employee'
+  },
+  {
+    routeLink :'q',
+    icon : 'fal fa-address-card',
+    label : 'Dashboard'
+  },
+] ;
+
